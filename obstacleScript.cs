4849724f63bb46da11a80c07b38f56a7cb6b6590
Jonathan Chen <58317363+jonathanchen1122@users.noreply.github.com>
2021-01-28 12:28:12 -0800

    Create obstacleScript.cs

---
+++ obstacleScript.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class obstacleScript : MonoBehaviour
+{
+    public float speed;
+
+    void Update()
+    {
+        transform.Translate(Vector2.left * speed * Time.deltaTime);
+    }
+}
