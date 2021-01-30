48318ead893b0a64749c4296b8ff2eb3e4f9d1d6
Jonathan Chen <58317363+jonathanchen1122@users.noreply.github.com>
2021-01-29 23:13:27 -0800

    Update managerScript.cs

--- managerScript.cs
+++ managerScript.cs
@@ -19,4 +19,9 @@ public class managerScript : MonoBehaviour
     {
         SceneManager.GetActiveScene();
     }
+
+    public void goBack()
+    {
+        SceneManager.LoadScene("Menu");
+    }
 }
