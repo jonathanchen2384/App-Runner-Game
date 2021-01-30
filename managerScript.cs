503f0566c25dc2cf31426ff33dbb072dc404911a
Jonathan Chen <58317363+jonathanchen1122@users.noreply.github.com>
2021-01-29 22:55:31 -0800

    Create managerScript.cs

---
+++ managerScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class managerScript : MonoBehaviour
+{
+    public void playGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void selectChar()
+    {
+        SceneManager.LoadScene("Select");
+    }
+
+    public void restart()
+    {
+        SceneManager.GetActiveScene();
+    }
+}
