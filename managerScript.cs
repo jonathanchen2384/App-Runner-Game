930b3b8418d0509b1884719190007efb78fa6543
Jonathan Chen <58317363+jonathanchen1122@users.noreply.github.com>
2021-01-30 11:44:58 -0800

    Update managerScript.cs

--- managerScript.cs
+++ managerScript.cs
@@ -8,20 +8,30 @@ public class managerScript : MonoBehaviour
     public void playGame()
     {
         SceneManager.LoadScene("Game");
+        Time.timeScale = 1;
     }
 
     public void selectChar()
     {
         SceneManager.LoadScene("Select");
+        Time.timeScale = 1;
     }
 
     public void restart()
     {
-        SceneManager.GetActiveScene();
+        SceneManager.LoadScene("Game");
+        Time.timeScale = 1;
     }
 
     public void goBack()
     {
         SceneManager.LoadScene("Menu");
+        Time.timeScale = 1;
+    }
+
+    public void goCredits()
+    {
+        SceneManager.LoadScene("Credits");
+        Time.timeScale = 1;
     }
 }
