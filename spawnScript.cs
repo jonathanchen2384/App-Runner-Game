d7dec0fe18080ad8bdb9234d01ba412c8ea7ac4f
Jonathan Chen <58317363+jonathanchen1122@users.noreply.github.com>
2021-01-30 21:51:08 -0800

    Update spawnScript.cs

--- spawnScript.cs
+++ spawnScript.cs
@@ -4,26 +4,52 @@ using UnityEngine;
 
 public class spawnScript : MonoBehaviour
 {
-    public GameObject obstacles;
+    public GameObject[] obstacles;
+    public GameObject powerUp;
 
     public float startTime;
     private float duration;
 
+    private float level;
+    private int count;
+
+    private bool enterBoss;
+
     void Start()
     {
+        enterBoss = false;
+        count = 0;
         duration = startTime;
+        level = 1;
     }
 
     void Update()
     {
-        if (duration <= 0)
+        if (count%10 == 0 && count != 0)
         {
-            Instantiate(obstacles, transform.position, Quaternion.identity);
-            duration = startTime;
+            level = 1.5f;
+        }
+
+        if (duration <= 0 && enterBoss == false)
+        {
+            if (level == 1) // normal
+            {
+                Instantiate(obstacles[(int)Random.Range(0, obstacles.Length)], transform.position, Quaternion.identity);
+                count++;
+                duration = startTime;
+            }
+
+            if (level == 1.5f) //power UP
+            {
+                Instantiate(powerUp, transform.position, Quaternion.identity);
+                count++;
+                level = 1;
+                duration = startTime;
+            }
         }
 
         else {
-            duration -= Time.deltaTime;
+           duration -= Time.deltaTime;
         }
     }
 }
