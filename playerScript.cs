2d09888f7fa1d748985a875f159a9488f1fafa85
Jonathan Chen <58317363+jonathanchen1122@users.noreply.github.com>
2021-02-05 17:44:54 -0800

    Update playerScript.cs

--- playerScript.cs
+++ playerScript.cs
@@ -8,17 +8,6 @@ public class playerScript : MonoBehaviour
 
     public GameObject[] player;
 
-    public Rigidbody2D RB;
-    public float jumpForce;
-
-    private bool isGrounded;
-    public Transform feetPos;
-    public float radius;
-    public LayerMask ground;
-
-    public int totalJumps;
-    private int extrajumps;
-
     //hearts
     public int health;
     public int numHearts;
@@ -29,52 +18,28 @@ public class playerScript : MonoBehaviour
     // pause button
     public GameObject overScene;
     public GameObject pauseObj;
+
     private Vector2 mousePos;
 
-    bool isClick;
+    public bool isClick;
     public Transform[] checkerPos;
 
-    //steps
-    public float startTime;
-    private float duration;
-    public GameObject stepOne;
-
-    private float durationTwo;
-    public GameObject stepTwo;
-
-    // jump Sound
-    public GameObject jumpSound;
-
     public int playerID;
-
-
-    public Animator Anim;
-
-
 
     public void Awake()
     {
         numHearts = PlayerPrefs.GetInt("Hearts", 3);
-        health = numHearts;
-
         playerID = PlayerPrefs.GetInt("Power", 1);
     }
 
     void Start()
     {
-        Anim.SetBool("isJumping", false);
-
+        health = numHearts;
 
         for (int i = 0; i < player.Length; i++)
         {
             player[i].SetActive(false);
         }
-
-        extrajumps = totalJumps;
-        isClick = true;
-
-        duration = startTime;
-        durationTwo = startTime * 1.5f;
     }
 
     private void FixedUpdate()
@@ -88,7 +53,7 @@ public class playerScript : MonoBehaviour
             isClick = false;
         }
 
-        //atack position
+        //attack position
 
         if (mousePos.x >= checkerPos[1].position.x && mousePos.y >= checkerPos[1].position.y)
         {
@@ -104,43 +69,12 @@ public class playerScript : MonoBehaviour
         {
             isClick = true;
         }
-
-        //footsteps
-        if (isGrounded == true)
-        {
-            RunSound();
-            Anim.SetBool("isJumping", false);
-        }
     }
 
     void Update()
     {
         identifyPlayer();
-
-        isGrounded = Physics2D.OverlapCircle(feetPos.position, radius, ground);
-
-        if (isGrounded == true && Input.GetMouseButtonDown(0))
-        { // jump
-            if (isClick == true)
-            {
-                Instantiate(jumpSound, transform.position, Quaternion.identity);
-                extrajumps = totalJumps;
-                RB.velocity = Vector2.up * jumpForce;
-                Anim.SetBool("isJumping", true);
-            }
-        }
-
-        if (isGrounded == false && extrajumps > 0)
-        {
-            if (Input.GetMouseButtonDown(0) && isClick == true)
-            {
-                Instantiate(jumpSound, transform.position, Quaternion.identity);
-                RB.velocity = Vector2.up * jumpForce;
-                extrajumps--;
-                Anim.SetBool("isJumping", true);
-            }
-        }
-
+        powerUpChecker();
         playerHealth();
 
         if (health <= 0)
@@ -149,23 +83,17 @@ public class playerScript : MonoBehaviour
             Instantiate(overScene, transform.position, Quaternion.identity);
             gameObject.SetActive(false);
         }
-
-        powerUpChecker();
     }
 
-    private void OnDrawGizmosSelected()
+    public void powerUpChecker()
     {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(feetPos.position, radius);
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.CompareTag("Obstacle") && health > 0)
+        if (GetComponent<collectionScript>().powered == true)
         {
-            health--;
+            health = numHearts;
+            GetComponent<collectionScript>().powered = false;
         }
     }
+
 
 
     public void playerHealth()
@@ -196,36 +124,6 @@ public class playerScript : MonoBehaviour
             {
                 hearts[i].enabled = false;
             }
-        }
-    }
-
-
-    public void powerUpChecker()
-    {
-        if (GetComponent<collectionScript>().powered == true)
-        {
-            health = numHearts;
-            GetComponent<collectionScript>().powered = false;
-        }
-    }
-
-
-    public void RunSound()
-    {
-        duration -= Time.deltaTime;
-        durationTwo -= Time.deltaTime;
-
-
-        if (duration <= 0) //step One
-        {
-            Instantiate(stepOne, transform.position, Quaternion.identity);
-            duration = startTime;
-        }
-
-        if (durationTwo <= 0) //step Two
-        {
-            Instantiate(stepTwo, transform.position, Quaternion.identity);
-            durationTwo = startTime;
         }
     }
 
